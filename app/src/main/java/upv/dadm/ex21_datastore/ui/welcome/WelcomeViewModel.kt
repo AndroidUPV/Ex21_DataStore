62213f6e5f632a014259b317d8e507ffc648bb97
ddandres <ddandres@disca.upv.es>
2024-01-29 15:24:14 +0100

    Update to use StateFlow instead of LiveData

--- app/src/main/java/upv/dadm/ex21_datastore/ui/welcome/WelcomeViewModel.kt
+++ app/src/main/java/upv/dadm/ex21_datastore/ui/welcome/WelcomeViewModel.kt
@@ -11,8 +11,15 @@
 
 package upv.dadm.ex21_datastore.ui.welcome
 
-import androidx.lifecycle.*
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import upv.dadm.ex21_datastore.data.welcome.WelcomePreferencesRepository
 import javax.inject.Inject
@@ -27,37 +34,43 @@ class WelcomeViewModel @Inject constructor(
 ) : ViewModel() {
 
     // Backing property for displaying the initial dialog (default empty until initialised)
-    private val _showInitialDialog = MutableLiveData<Boolean>()
+    private val _showInitialDialog = MutableStateFlow(false)
 
     // Whether to show or hide the initial dialog
-    val showInitialDialog: LiveData<Boolean>
-        get() = _showInitialDialog
+    val showInitialDialog = _showInitialDialog.asStateFlow()
 
     // Initialise the property for displaying the initial dialog
     init {
         // As it is a blocking operation it should be executed in a thread
         viewModelScope.launch {
             // Get the value from the repository
-            _showInitialDialog.value =
+            _showInitialDialog.update {
                 welcomePreferencesRepository.getInitialDialogVisibility().isDialogVisible
+            }
         }
     }
 
     // Whether to show the display or hide option menu
     // according to the preference selected by the user for displaying the initial dialog
-    val showDialogMenu: LiveData<Boolean> =
-        // Transform the received Flow into LiveData to update the UI in a lifecycle aware manner
-        welcomePreferencesRepository.getDialogVisibility().asLiveData().map { prefs ->
+    val showDialogMenu =
+        // Transform the received Flow into StateFlow to update the UI in a lifecycle aware manner
+        welcomePreferencesRepository.getDialogVisibility().map { prefs ->
             prefs.isDialogVisible
-        }
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(),
+            initialValue = true
+        )
 
     /**
-     * Sets the preference to display the initial dialog according to the user selection.
+     * Sets the preference to display the initial dialog according to the user selection and
+     * hides the initial dialog.
      */
     fun setDialogVisibility(isVisible: Boolean) {
         // As it is a blocking operation it should be executed in a thread
         viewModelScope.launch {
             welcomePreferencesRepository.setDialogVisibility(isVisible)
+            _showInitialDialog.update { false }
         }
     }
 
